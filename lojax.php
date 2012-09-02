f067813c53d8ad6585433c8d636b630170bd7ea7
dozen <kyosuke6428@gmail.com>
2012-09-02 22:18:36 +0900

    bugfix

--- lojax.php
+++ lojax.php
@@ -354,9 +354,9 @@ $expose = false;
 $attrs = ($expose ? 'rows="10" cols="30"' : 'rows="1" cols="1" disabled="disabled"');
 $type = ($expose ? 'type="text"' : 'type="hidden"');
 $html = '<form action="" id="lojax_sender"><fieldset>'
-	. '<textarea id="lojax_response" ' . $attrs . '>' . (isset($output) ? htmlspecialchars($output, ENT_QUOTES) : '') . '</textarea>'
-	. '<textarea id="lojax_headers" ' . $attrs . '>' . (isset($headers) ? htmlspecialchars($headers, ENT_QUOTES) : '') . '</textarea>'
-	. '<input id="lojax_status" ' . $type . ' value="' . (isset($status) ? htmlspecialchars($status, ENT_QUOTES) : '') . '" />'
+	. '<textarea id="lojax_response" ' . $attrs . '>' . (isset($output) ? htmlspecialchars($output, ENT_QUOTES, 'UTF-8') : '') . '</textarea>'
+	. '<textarea id="lojax_headers" ' . $attrs . '>' . (isset($headers) ? htmlspecialchars($headers, ENT_QUOTES, 'UTF-8') : '') . '</textarea>'
+	. '<input id="lojax_status" ' . $type . ' value="' . (isset($status) ? htmlspecialchars($status, ENT_QUOTES, 'UTF-8') : '') . '" />'
 	. '</fieldset></form>';
 
 //output the form
